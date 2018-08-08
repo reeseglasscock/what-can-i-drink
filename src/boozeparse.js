fe8a6c7aebd90e2f47125e7d78890583b8cfdb1a
Reese Glasscock and Ryan Murry <reese@>
2018-08-08 16:50:26 -0700

    add comment to boozeparse

--- src/boozeparse.js
+++ src/boozeparse.js
@@ -14,6 +14,7 @@ export class BoozeParse {
 
   getDrinkInfo(response) {
     let body = JSON.parse(response);
+    //run loop to get all ingredients at [index] and then add in array to Instructions
     let foundInstruction= new Instruction(body.drinks[0].strGlass, body.drinks[0].strInstructions, body.drinks[0].strIngredient1);
     return foundInstruction;
   }
